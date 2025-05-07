3d133542eb1fbf1dc55749a35e0cf33b64cb1f68
= <=>
2025-05-07 16:29:02 +0100

    updated the project

---
+++ wek1/day2/exercice/exercice.py
@@ -0,0 +1,9 @@
+# exercice 1
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+# Utilisation de zip pour associer les clés et les valeurs
+result_dict = dict(zip(keys, values))
+
+# Afficher le dictionnaire résultant
+print(result_dict)
